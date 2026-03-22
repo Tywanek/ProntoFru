7fd39130555e406ea6bdac5f66c54e05c61cacbb
Tywanek <Tywanek@users.noreply.github.com>
2026-03-22 15:27:07 +0100

    FlightRepository

---
+++ composeApp/src/commonMain/kotlin/com/radlab/prontofru/domain/model/Flight.kt
@@ -0,0 +1,12 @@
+package com.radlab.prontofru.domain.model
+
+import kotlinx.datetime.LocalDateTime
+
+data class Flight(
+    val id: String,
+    val price: Double,
+    val origin: String,
+    val destination: String,
+    val departureDate: LocalDateTime,
+    val returnDate: LocalDateTime,
+)
