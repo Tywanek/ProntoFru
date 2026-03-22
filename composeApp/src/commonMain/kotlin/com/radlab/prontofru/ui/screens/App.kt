a119aa6535a178d8b93417b8e2829a51a2965a5e
Tywanek <Tywanek@users.noreply.github.com>
2026-03-22 15:24:35 +0100

    setup MVVM

--- composeApp/src/commonMain/kotlin/com/radlab/prontofru/ui/screens/App.kt
+++ composeApp/src/commonMain/kotlin/com/radlab/prontofru/ui/screens/App.kt
@@ -1,4 +1,4 @@
-package com.radlab.prontofru
+package com.radlab.prontofru.ui.screens
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.radlab.prontofru.Greeting
 import org.jetbrains.compose.resources.painterResource
 
 import prontofru.composeapp.generated.resources.Res
